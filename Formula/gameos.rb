8720713f2b5ae8b5944fa609ba1cfe22fc6d64c3
aditya-elastic <aditya@elasticdevs.io>
2026-06-02 11:55:27 +0530

    Add Game OS formula

---
+++ Formula/gameos.rb
@@ -0,0 +1,18 @@
+class Gameos < Formula
+  desc "Pure CLI local AI game studio runtime"
+  homepage "https://github.com/aditya-elastic/gameos"
+  url "https://registry.npmjs.org/gameos/-/gameos-0.1.0.tgz"
+  sha256 "7161a5f6313f36bad5cfc04499f67d6d45e807079c7fa69c011b7e889cd10bbd"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args(prefix)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gameos --version")
+    assert_match '"telemetry": false', shell_output("#{bin}/gameos doctor --json")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew Game OS Tap
+
+Install the Game OS CLI:
+
+```bash
+brew tap aditya-elastic/gameos
+brew install gameos
+```
+
+The formula installs the public npm package `gameos`.
